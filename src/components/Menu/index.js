6d0011d11530d605c62db9e14af358244d637af7
Jacqueline Dean <jacquelineadean@gmail.com>
2020-08-17 15:06:46 -0500

    Added About content

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from '../../pages/about';
 import './style.css';
 
 export default class Menu extends Component {
@@ -14,20 +16,22 @@ export default class Menu extends Component {
     
     render() {
         return (
-            <section className="menu">
-                <div ref={this.wrapperRef} className="wrapper">
-                    <div className="nav">
-                        <span className="menu_enter" type="menu-fold" onClick={() => this.handleClick()}> - Menu </span> 
-                        <div className="nav__body">
-                            <ul>
-                                <li>About</li>
-                                <li>Porfolio</li>
-                                <li>Resume</li>
-                            </ul>
+            <Router>
+                <section className="menu">
+                    <div ref={this.wrapperRef} className="wrapper">
+                        <div className="nav">
+                            <span className="menu_enter" type="menu-fold" onClick={() => this.handleClick()}> - Menu </span> 
+                            <div className="nav__body">
+                                <ul>
+                                    <li><a href={About}>About</a></li>
+                                    <li>Porfolio</li>
+                                    <li>Resume</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </Router>
         );
     }
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
+import React, { Component } from 'react';
+// import InfiniteScroll from 'react-infinite-scroll-component';
+import image from './FullSizeRender.jpeg'
+import './style.css';
 
-function About() {
+class About extends Component {
     // const items = `"Without continual growth and progress, such words as improvement, achievement, and success have no meaning." - Benjamin Franklin`
-    return (
-        <div>
-            
-        </div>
-    )
+    render() {
+        return (
+            <section className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <img src={image} alt="profile" className="card-img profile-picture"/>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="text-box">
+                            <h3 className="card-title">About</h3>
+                            <p className="card-text">
+                                I am a Software Engineer passionate about building dynamic applications with attention to scalability. 
+                                Originally from Georgia, I currently live in Austin, Texas where I recently graduated from the University of Texas with a certificate in Full Stack Web Development.
+                                A few of my favorite technologies to work with include React, Node, and SQL/noSQL databases, but I am committed to continuous development and excited to further advance my repertoire of languages and technologies.
+                            </p>
+                            <p className="card-text">
+                                When I'm not programming, I try to get away from the screen and get outside. My favorite activities all involve the great outdoors. Whether it's going for a run, hiking, kayaking, or just
+                                having a picnic in the park, I am happiest when I'm getting fresh air in the sun.
+                            </p>
+                            {/* <a data-toggle="modal" data-target="#ModalPDF" className="btn">Resume</a> */}
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
 }
 
 export default About;
